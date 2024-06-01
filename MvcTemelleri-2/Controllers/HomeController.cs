b25ed7a3d2e7d21e483b97d7b399b90560f845ed
aribilgiogretmen <aribilgiogretmen@gmail.com>
2024-06-01 11:07:16 +0300

    Add project files.

---
+++ MvcTemelleri-2/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcTemelleri_2.Models;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace MvcTemelleri_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            
+           
+            
+            return View(new Calc());
+        }
+
+        [HttpPost]
+        public IActionResult Index(Calc model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Sum = model.Num1 + model.Num2;
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MvcTemelleri-2/Models/Calc.cs
@@ -0,0 +1,10 @@
+﻿namespace MvcTemelleri_2.Models
+{
+    public class Calc
+    {
+        public int? Num1 { get; set; }
+        public int? Num2 { get; set; }
+        public int? Sum { get; set; }
+
+    }
+}
